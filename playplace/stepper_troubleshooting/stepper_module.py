e1140f4862b039d92919b65310f852043e375b63
Prof. Willis' RPi 4 <arfarah2002@gmail.com>
2021-06-30 16:47:26 -0400

    stepper module adjustments

--- playplace/stepper_troubleshooting/stepper_module.py
+++ playplace/stepper_troubleshooting/stepper_module.py
@@ -1,5 +1,6 @@
 import time 
 import RPi.GPIO as GPIO
+import numpy as np
 
 from RpiMotorLib import RpiMotorLib
 
@@ -10,22 +11,21 @@ class StepperMotor(object):
 	drive_conversions = {"half": 0.5,
 						 "full": 1.0}
 
-	def __init__(self, GPIO_pins, drive="half", CONT_BOOL=False, init_delay=0.0):
+	def __init__(self, GPIO_pins, drive="half", CONT_BOOL=False, init_delay=0.000001):
 		self.motor = RpiMotorLib.BYJMotor("stepper_motor", "28BYJ")
 		self.GPIO_pins = GPIO_pins
 		self.drive = drive
 		# initial position is 0 (make sure to calibrate manually IRL!!)
 		self.angles_list = [0] 
 		self.CONTINUOUS_BOOL = CONT_BOOL 
-		self.VERB_BOOL = True
+		self.VERB_BOOL = False
 		self.init_delay = init_delay
 
 	def turn_to_angle(self, speed_pct, angle=None, d_angle=None):
 		# Bring angle back between 0 and 360 if you don't want continuous rotation
-
-		if angle:
+		if type(angle) == float:
 			main_angle = angle
-		if d_angle:
+		if type(d_angle) == float:
 			main_angle = d_angle 
 
 		if self.CONTINUOUS_BOOL:
@@ -34,18 +34,22 @@ class StepperMotor(object):
 			while main_angle > 360:
 				main_angle -= 360
 
-		if angle:
+		if type(angle) == float:
 			self.angles_list.append(main_angle)
 			angle_diff = self.angles_list[-1] - self.angles_list[-2]
-		if d_angle:
+		if type(d_angle) == float:
 			angle_diff = main_angle
 
 		# calculate the number of steps from the angle
 		steps = abs(angle_diff)*512./360.
 
 		# Delay (half vs full)
-		delay = 0.0009*self.drive_conversions[self.drive]/(speed_pct/100)
-		# print(delay)
+		min_delay = {True:0.0009, 
+					 False:0.0013}
+		delay = 0.0014*self.drive_conversions[self.drive]/(speed_pct/100)
+
+		# print(min_delay[self.VERB_BOOL])
+		# time.sleep(10)
 
 		# If angle diff less than 0, turn clockwise
 		if angle_diff >= 0:
@@ -65,14 +69,27 @@ class StepperMotor(object):
 if __name__ == '__main__':
 	GPIO_pins = [17,27,22,23]
 	stroke_plane_servo = StepperMotor(GPIO_pins, CONT_BOOL=False)
-	# speed = 1.
+	# speed_pct = 100
 	# angle = 90
-	d_angle = 90
-	while True:
-		speed_pct = float(input("Speed percentage: "))
+	# d_angle = 90
+	# while True:
+		# speed_pct = float(input("Speed percentage: "))
 		# angle = float(input("Angle: "))
 		# stroke_plane_servo.turn_to_angle(speed_pct, angle=angle)
 		# d_angle = float(input("d_angle: "))
-		stroke_plane_servo.turn_to_angle(speed_pct, d_angle=d_angle)
+		# stroke_plane_servo.turn_to_angle(speed_pct, d_angle=d_angle)
+
+	try:
+		t_list = np.linspace(0,2*np.pi,100)
+		for t in t_list:
+			angle = float(180*np.sin(t))
+			print(angle)
+			speed_pct = float(40*np.cos(t) + 50.)
+			# speed_pct = 100
+
+			stroke_plane_servo.turn_to_angle(speed_pct, angle=angle)
+	except KeyboardInterrupt:
+		exit()
+
 
 	GPIO.cleanup()
