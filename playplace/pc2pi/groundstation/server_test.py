421f3a44eb9a303f230229f097ba3db01e78af9d
Albert Farah <arfarah2002@gmail.com>
2021-07-15 14:23:57 -0400

    outlined pc main, added communicate

--- playplace/pc2pi/groundstation/server_test.py
+++ playplace/pc2pi/groundstation/server_test.py
@@ -5,8 +5,8 @@ import numpy as np
 
 class SocketServer(object):
 
-	def __init__(self, pi_IP_address, port=50007):
-		self.HOST = pi_IP_address
+	def __init__(self, server_IP_address, port=50007):
+		self.HOST = server_IP_address
 		self.PORT = port
 		self.gui_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.gui_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -21,19 +21,19 @@ class SocketServer(object):
 	def close(self):
 		self.gui_socket.close()
 
-if __name__ == '__main__':
-	HOST = '192.168.1.95'
-	# PORT = 50007
-	gui_server = SocketServer(HOST)
-	arr = {"joint torques" : [0, [1,2,3,4,5,6]], 
-			"stroke plane"  : [0, [1]]}
-	while 1:
-		arr["joint torques"][0] += .1
-		arr["joint torques"][1] = np.sin(arr["joint torques"][0] + np.random.rand(6))
-		arr["stroke plane"][0] += .1
-		arr["stroke plane"][1] = np.cos(arr["stroke plane"][0] + np.random.rand(1))
+# if __name__ == '__main__':
+# 	HOST = '192.168.1.95'
+# 	# PORT = 50007
+# 	gui_server = SocketServer(HOST)
+# 	arr = {"joint torques" : [0, [1,2,3,4,5,6]], 
+# 			"stroke plane"  : [0, [1]]}
+# 	while 1:
+# 		arr["joint torques"][0] += .1
+# 		arr["joint torques"][1] = np.sin(arr["joint torques"][0] + np.random.rand(6))
+# 		arr["stroke plane"][0] += .1
+# 		arr["stroke plane"][1] = np.cos(arr["stroke plane"][0] + np.random.rand(1))
 
-		print(arr)
-		gui_server.send_data_pack(arr)
-		time.sleep(0.02)
-	gui_server.close()
+# 		print(arr)
+# 		gui_server.send_data_pack(arr)
+# 		time.sleep(0.02)
+# 	gui_server.close()
