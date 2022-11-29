38074126690b0263648d9dd98232fd6edf633c0a
Albert Farah <arfarah2002@gmail.com>
2022-11-28 23:40:36 -0500

    isaac lifting line - force apply works, time to debug vectors and stuff :D

--- isaacflapper3/tasks/bird3.py
+++ isaacflapper3/tasks/bird3.py
@@ -1,3 +1,5 @@
+
+
 import math
 import numpy as np
 import os
@@ -52,11 +54,11 @@ class Bird(VecTask):
 		self.gym.refresh_dof_state_tensor(self.sim)
 
 		# control tensors
-		self.N = 15
-
-		self.forces = torch.zeros((self.num_envs, self.num_bodies, 3), dtype=torch.float32, device=self.device, requires_grad=False)
-		self.torques = torch.zeros((self.num_envs, self.num_bodies, 3), dtype=torch.float32, device=self.device, requires_grad=False)
-
+		self.N = 10
+		self.lforces = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
+		self.rforces = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
+		self.lnodes = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
+		self.rnodes = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
 		cam_pos = gymapi.Vec3(-2.0, -2.0, 1.0)
 		# cam_pos = gymapi.Vec3(0.0, -1.0, 1.0)
 		cam_target = gymapi.Vec3(0.0, 0.0, 1.0)
@@ -64,8 +66,9 @@ class Bird(VecTask):
 
 		self.t = 0
 
-		self.lifting_line_setup()
-
+		# if self.t == 0:
+		# 	init_state = to_torch([[0,0,1,0,0,0,0,-.01,0,0,0,0,0],[0,0,1,0,0,0,0,-.01,0,0,0,0,0]], device=self.device)
+		# 	self.gym.set_actor_root_state_tensor(self.sim, gymtorch.unwrap_tensor(init_state))
 
 	def create_sim(self):
 		self.sim = super().create_sim(self.device_id, self.graphics_device_id, self.physics_engine, self.sim_params)
@@ -315,255 +318,6 @@ class Bird(VecTask):
 
 		return wing_handle, r_nodes, dT_global, aux_data_pack
 
-	def make_trap_profile(self, c0, c1, s):
-
-		y_n = torch.reshape((((torch.linspace(self.eps-1.0,1.0-self.eps,self.N, device=self.device))**2)**0.6)**0.5,[self.N,1]); # "station" locs
-		idx = torch.tensor(torch.floor(self.N/2), dtype=torch.int32, device=self.device)
-		y_n[0:idx,0] = -y_n[0:idx,0]
-		y = torch.tensor(s*y_n, dtype=torch.float32,device=self.device)
-		c = (y+s)*((c1-c0)/2.0/s)+c0
-
-		return torch.unsqueeze(c, 0), torch.unsqueeze(y, 0)
-
-	def lifting_line_setup(self):
-
-		params = {"eps" : 1e-3,         # spacing from wing ends
-				  "wings" : 2,           # number of wings
-				  "cla" : 6.5,             # cla
-				  "rho" : 1000,             # density
-				  "S1" : 0.075,              # wing 1 semispan
-				  "S2" : 0.075,              # wing 2 semispan                  
-				  "C1" : [0.1, 0.1],              # wing 1 chord
-				  "C2" : [0.1, 0.1]}             # wing 2 chord
-
-		self.eps = torch.tensor(params["eps"], device=self.device)
-		self.N = torch.tensor(self.N, device=self.device)
-		self.W = torch.tensor(params["wings"], device=self.device)
-		self.M = torch.tensor(self.num_envs, device=self.device)
-		self.Cla = torch.tensor(params["cla"], device=self.device)
-		self.S1 = torch.tensor(params["S1"], device=self.device)
-		self.S2 = torch.tensor(params["S2"], device=self.device)
-		self.C1 = torch.tensor(params["C1"], device=self.device)
-		self.C2 = torch.tensor(params["C2"], device=self.device)
-
-		self.s = torch.reshape(torch.tensor((params["S1"],params["S2"]), device=self.device), [2,1,1])
-
-		C1, Y1 = self.make_trap_profile(params["C1"][0], params["C1"][1], self.s[0])
-		C2, Y2 = self.make_trap_profile(params["C2"][0], params["C2"][1], self.s[1])
-
-		self.C = torch.cat([C1, C2], dim=0)
-		self.Y = torch.cat([Y1, Y2], dim=0) 
-
-		self.theta = torch.acos(self.Y/self.s)
-		self.vec1 = torch.sin(self.theta)*self.C*self.Cla/8.0/self.s
-
-		self.n = torch.reshape(torch.linspace(1,self.N,self.N, dtype=torch.float32, device=self.device),[1,self.N])
-		self.mat1 = (self.n*self.C*self.Cla/8.0/self.s + torch.sin(self.theta))*torch.sin(self.n*self.theta)
-		self.mat2 = 4.0*self.s*torch.sin(self.n*self.theta)
-		# Used in drag calculation 
-		self.mat3 = torch.sin(self.n*self.theta)
-		self.vec3 = torch.tensor(torch.reshape(torch.arange(1,self.N+1,device=self.device), (self.N,1))/torch.sin(self.theta), dtype=torch.float, device=self.device)
-
-		self.force_scale = torch.squeeze(2*self.s/(self.N), dim=-1)
-
-	def lifting_line(self):
-
-		s = 0.15 # span 
-		# self.N = 5 # num of nodes #<----bug: force function throws error with stations neq than 5?
-		self.rho = 1000 # fluid density
-		c = 0.1 # chord
-		dr = s/self.N
-
-		COM_wing_1 = None
-		COM_wing_2 = None
-
-		psi_wing1 = None
-		psi_wing2 = None
-
-		v_flow_2D_local_mag_wing_1 = None
-		v_flow_2D_local_mag_wing_2 = None
-
-		v_flow_3D_global_wing_1 = None
-		v_flow_3D_global_wing_2 = None	
-
-		global_x_i_wing_1 = None
-		global_x_i_wing_2 = None
-
-		nodes_wing_1 = None
-		nodes_wing_2 = None
-
-		for wing in ["left", "right"]:
-
-			wing_handle = {"left" : self.lwing_handle, "right" : self.rwing_handle}[wing]
-
-			wing_pos = self.rb_pos[:,wing_handle,:]
-			wing_rot = self.rb_rot[:,wing_handle,:] # xyzw
-			wing_linvel = self.rb_linvel[:,wing_handle,:]
-			wing_angvel = self.rb_angvel[:,wing_handle,:]
-
-			# print(wing_pos[0,:])
-
-			# unit vectors
-			x_i = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
-			y_i = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
-			z_i = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
-			
-			x_i[:,:,0] = 1.0
-			y_i[:,:,1] = 1.0
-			z_i[:,:,2] = 1.0
-
-
-			# torch3d quaternion uses wxyz, must roll isaac xyzw quaternion
-			wing_rot_wxyz = torch.roll(wing_rot, 1, 1)
-
-			rot_mat = quaternion_to_matrix(wing_rot_wxyz)
-			global_x_i = torch.matmul(rot_mat, x_i.transpose(1,2)).transpose(1,2)
-
-			center_node_offset = s/2 + 0.1
-			R_nodes = torch.zeros((self.num_envs,self.N,3), dtype=torch.float32, device=self.device, requires_grad=False)
-			R_nodes[...,0] = torch.linspace(center_node_offset + -s/2,center_node_offset + s/2,self.N)
-
-			# the wing pos is not the COM, offset it a little bit along the span to correct it
-			wing_pos_offset = torch.matmul(rot_mat, (s/2+.1)*x_i.transpose(1,2)).transpose(1,2)[:,0,:] 
-			wing_pos += wing_pos_offset
-
-			r_nodes = torch.matmul(rot_mat, R_nodes.transpose(1,2)).transpose(1,2) \
-						+ torch.tile(wing_pos, (1,self.N)).reshape(self.num_envs, self.N, 3)
-
-
-			v_nodes_about_link_origin = torch.cross(torch.tile(wing_angvel, (1,self.N)).reshape(self.num_envs, self.N, 3), 
-												r_nodes, dim=2)
-
-			# wing_angvel_local = torch.bmm(torch.inverse(rot_mat), wing_angvel[..., None]) # in local frame
-			# # get vel about local frame origin in LOCAL frame, then transform to global
-			# V_nodes_about_link_origin = torch.cross(torch.tile(wing_angvel_local, (1,self.N)).reshape(self.num_envs, self.N, 3), 
-			# 									R_nodes, dim=2)
-			# v_nodes_about_link_origin = torch.matmul(rot_mat, V_nodes_about_link_origin.transpose(1,2)).transpose(1,2)
-
-
-			v_nodes = torch.add(torch.tile(wing_linvel, (1,self.N)).reshape(self.num_envs, self.N, 3),
-								v_nodes_about_link_origin)
-
-			v_flow_3D_global_wing = -v_nodes
-
-
-			v_flow_3D_local = torch.matmul(torch.inverse(rot_mat), v_flow_3D_global_wing.transpose(1,2)).transpose(1,2)
-
-			v_flow_2D_local = v_flow_3D_local.detach().clone()
-			v_flow_2D_local[:,:,0] = 0.0
-			v_flow_2D_local_mag_wing = torch.linalg.norm(v_flow_2D_local, axis=2)
-
-			psi_wing = torch.atan2(v_flow_2D_local[:,:,2],(-1)**(wing_handle+1)*v_flow_2D_local[:,:,1])
-
-			if wing == "left":
-				COM_wing_1 = wing_pos
-				psi_wing1 = psi_wing
-				v_flow_2D_local_mag_wing_1 = v_flow_2D_local_mag_wing
-				v_flow_3D_global_wing_1 = v_flow_3D_global_wing
-				global_x_i_wing_1 = global_x_i
-				nodes_wing_1 = r_nodes
-
-			if wing == "right":
-				COM_wing_2 = wing_pos
-				psi_wing2 = psi_wing
-				v_flow_2D_local_mag_wing_2 = v_flow_2D_local_mag_wing
-				v_flow_3D_global_wing_2 = v_flow_3D_global_wing
-				global_x_i_wing_2 = global_x_i
-				nodes_wing_2 = r_nodes
-
-
-		# print(psi_wing1.shape, psi_wing2.shape)
-
-		# stack psis, v 2d local mags
-		psi = torch.stack([psi_wing1, psi_wing2])
-		self.r_wing_COM = torch.stack([COM_wing_1, COM_wing_2]).transpose(0,1)
-		self.vec2 = psi.transpose(1,2)
-		v_flow_2D_local_mag = torch.stack([v_flow_2D_local_mag_wing_1, v_flow_2D_local_mag_wing_2]).transpose(1,2)
-		v_flow_3D_global = torch.stack([v_flow_3D_global_wing_1, v_flow_3D_global_wing_2])
-		r_nodes = torch.stack([nodes_wing_1, nodes_wing_2]).transpose(0,1)
-
-		###########################################################################################################
-		###########################################################################################################
-
-		# print(self.vec1.shape, self.vec2.shape)
-		RHS = self.vec1*self.vec2
-
-		self.RHS = RHS
-		A = torch.linalg.solve(self.mat1,RHS)   
-		# A = torch.matmul(mat1inv,RHS)  
-		# each col in above will have the "A" coeffs for the mth wing 
-		Gamma = torch.matmul(self.mat2,A)*v_flow_2D_local_mag
-		LiftDist = Gamma*v_flow_2D_local_mag*self.rho
-
-
-		# exec_time = time.perf_counter() - now; 
-		Alpha_i = torch.matmul(self.mat3, A * self.vec3) 
-		DragDist = v_flow_2D_local_mag*Gamma*Alpha_i*self.rho
-		DragDist = DragDist.type(torch.float)
-
-		########### LIFT AND DRAG UNIT VECTORS
-		#  LIFT = global flow X global span 
-		# stack x_i for 2 wings
-		spanwise_vector = torch.stack([global_x_i_wing_1, global_x_i_wing_2])
-
-		Direction_Of_Lift = torch.cross(spanwise_vector, v_flow_3D_global, dim=3)
-
-		Direction_Of_Lift = torch.div(Direction_Of_Lift, torch.linalg.norm(Direction_Of_Lift, axis=3).reshape(2, self.num_envs, self.N, 1))
-		
-		#  DRAG = UNIT vector of 2d global flow
-		Direction_Of_Drag = torch.div(v_flow_3D_global, torch.linalg.norm(v_flow_3D_global, axis=3).reshape(2, self.num_envs, self.N, 1))
-		###########
-
-		# F_global = Direction_Of_Lift*LiftDist + Wind_apparent_global_normalized*DragDist
-		LD1 = torch.permute(LiftDist, (2,0,1))*self.force_scale
-		LiftDist_ = torch.reshape(LD1, (self.W*self.M,self.N,1))
-
-		DD1 = torch.permute(DragDist, (2,0,1))*self.force_scale
-		DragDist_ = torch.reshape(DD1, (self.W*self.M,self.N,1))
-
-		DOL1 = torch.permute(Direction_Of_Lift, (0,2,1,3))
-		DOD1 = torch.permute(Direction_Of_Drag, (0,2,1,3))
-
-		Direction_Of_Lift_ = torch.reshape(DOL1, (self.W*self.M, self.N, 3))
-		Direction_Of_Drag_ = torch.reshape(DOD1, (self.W*self.M, self.N, 3))
-
-		self.DOL = Direction_Of_Lift
-		self.Lift_global = LiftDist_*Direction_Of_Lift_	
-		self.Drag_global = DragDist_*Direction_Of_Drag_
-		self.F_global = self.Lift_global + self.Drag_global        
-		# print("F_global: ", self.F_global[0,::])
-
-		self.F_global = torch.reshape(self.F_global, (self.M, 2, self.N, 3)) # global forces at each node
-
-		
-
-		#-----------------------------------------------------#
-		# Turning force distributions (lift + drag) into single forces and torques that act on each wing
-		# Sum forces, apply to COM
-		forces_sum = torch.sum(self.F_global, dim=2)
-		print("Sum forces: ", forces_sum[0,:])
-
-		# Calculate torque contribution from each force on each node
-
-		# Get vector from COM to node
-		r_wing_COM = self.r_wing_COM.repeat_interleave(self.N,dim=1).reshape(self.num_envs, self.W, self.N, 3)
-		r_COM_to_node = r_nodes - r_wing_COM
-
-		# Take r_COM_to_node X F_global (torque cont from each node)
-		torques = torch.cross(r_COM_to_node, self.F_global, dim=3)
-		# Sum torques, apply to COM
-		torques_sum = torch.sum(torques, dim=2)
-
-		# Assign these forces and torques to the selfforces/torques defined up top, in the 3rd and 4th rigid bodies (the wings)
-		self.forces[:,3:,:] = forces_sum
-		self.torques[:,3:,:] = torques_sum
-
-		#-----------------------------------------------------#
-
-		return forces_sum, self.r_wing_COM
-		###########################################################################################################
-		###########################################################################################################
-
 	def pre_physics_step(self, actions):
 
 		indices = to_torch([0,1], 
@@ -571,20 +325,20 @@ class Bird(VecTask):
 							device=self.device).repeat((self.num_envs, 1))
 
 
-		if self.t < 100:
-			self.root_linvel[:,1] = -1. #<--- this is how you set HoG control!
+		if self.t < 10000:
+			# self.root_linvel[:,1] = -1. #<--- this is how you set HoG control!
 			# self.root_linvel[:,0] = 0 #<--- this is how you set HoG control!
 			# self.root_angvel[:,:] = 0
-			# self.root_angvel[:,1] = 0
-			# self.root_angvel[:,2] = 0
+			self.root_angvel[:,1] = 0
+			self.root_angvel[:,2] = 0
 			self.gym.set_actor_root_state_tensor_indexed(self.sim, 
 														 gymtorch.unwrap_tensor(self.root_states),
 														 gymtorch.unwrap_tensor(indices), len(indices))
 
-		# _actions = [-1.*np.sin(5*self.t*0.01), 10*np.sin(5*self.t*0.01), 10*np.sin(5*self.t*0.01)]
+		_actions = [-1.*np.sin(5*self.t*0.01), 10*np.sin(5*self.t*0.01), 10*np.sin(5*self.t*0.01)]
 		# _actions = [0, 1*np.sin(10*self.t*0.01), 1*np.sin(10*self.t*0.01)]
-		# _actions = [0,-1,-1]
-		_actions = [-0.1,0,0]
+		# _actions = [0,-1,1]
+		# _actions = [-0.5,0,0]
 		# _actions = [-1.*np.sin(10*self.t*0.01), 0, 0]
 
 		actions = to_torch(_actions, 
@@ -594,23 +348,78 @@ class Bird(VecTask):
 		self.actions = actions.clone().to(self.device)
 		self.gym.set_dof_position_target_tensor(self.sim, gymtorch.unwrap_tensor(self.actions))
 
-		###################################### BEMT ##################################################
 		lwing_handle, self.lr_nodes, self.ldT, self.laux_data_pack = self.bemt2("left")
 		rwing_handle, self.rr_nodes, self.rdT, self.raux_data_pack = self.bemt2("right")
-		##################################### LIFTING LINE ###################################################
-		self.wing_forces, self.wing_CoMs = self.lifting_line()
 
-		if torch.isnan(torch.sum(self.forces)):
-			self.forces = torch.zeros_like(self.forces)
-			self.torques = torch.zeros_like(self.torques)
-			# dT = None
+		forces_rf = "env"
+		forces_app = True # apply forces?
 
-		##############################################################################################
+		if forces_rf == "local":
+			self.lforces = self.dt*self.laux_data_pack["forces local"]
+			self.rforces = self.dt*self.raux_data_pack["forces local"]
+			self.lnodes = self.laux_data_pack["nodes local"]
+			self.rnodes = self.raux_data_pack["nodes local"]
+			frame = gymapi.LOCAL_SPACE
 
-		# if self.t > 100:
-		# 	self.gym.apply_rigid_body_force_tensors(self.sim, gymtorch.unwrap_tensor(self.forces), 
-		# 													gymtorch.unwrap_tensor(self.torques), 
-		# 													gymapi.ENV_SPACE)
+		if forces_rf == "env":
+			self.lforces[...] = 0*self.dt*self.ldT
+			self.rforces[...] = self.dt*self.rdT
+			self.lnodes[...] = self.lr_nodes
+			self.rnodes[...] = self.rr_nodes
+			frame = gymapi.ENV_SPACE
+
+
+		print(self.lforces, "\n\n", self.rforces)
+
+		# print(gymtorch.wrap_tensor(gymtorch.unwrap_tensor(lforces.contiguous()))).view((self.num_envs, self.N, 3))
+
+		# if self.t > 2:
+		# 	exit()
+
+		# lforces = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
+		# rforces = torch.zeros((self.num_envs, self.N, 3), dtype=torch.float32, device=self.device, requires_grad=False)
+
+		# print(lnodes, "\n\n", rnodes)
+
+
+		if self.t == 3:
+			print(self.lforces.shape)
+			exit()
+
+		self.gym.apply_rigid_body_force_at_pos_tensors(self.sim, gymtorch.unwrap_tensor(self.lforces), 
+														gymtorch.unwrap_tensor(self.lnodes), 
+														frame)
+		self.gym.apply_rigid_body_force_at_pos_tensors(self.sim, gymtorch.unwrap_tensor(self.rforces), 
+														gymtorch.unwrap_tensor(self.rnodes), 
+														frame)
+
+		'''
+		if force_rf == "env":
+			forces = torch.cat((self.ldT, self.rdT), 1)
+			nodes = torch.cat((self.lr_nodes, self.rr_nodes), 1)
+			frame = gymapi.LOCAL_SPACE
+		if force_rf == "local":
+			forces = torch.cat((self.laux_data_pack["forces local"], 
+								self.raux_data_pack["forces local"]), 1)
+			nodes = torch.cat((self.laux_data_pack["nodes local"],
+							   self.raux_data_pack["nodes local"]), 1)
+			frame = gymapi.ENV_SPACE
+		self.gym.apply_rigid_body_force_at_pos_tensors(self.sim, 
+													   gymtorch.unwrap_tensor(forces),
+													   gymtorch.unwrap_tensor(nodes),
+													   frame)
+		'''
+
+		# ldT[...] = 0
+		# ldT[...,2] = 1
+		# exit()
+
+		# if self.t == 10:
+		# 	print("Applying force")
+		# 	time.sleep(1)
+		# 	self.gym.apply_rigid_body_force_at_pos_tensors(self.sim, gymtorch.unwrap_tensor(ldT.contiguous()), 
+		# 													gymtorch.unwrap_tensor(lr_nodes.contiguous()), 
+		# 													gymapi.self.ENV_SPACE)
 
 	def post_physics_step(self):
 		self.progress_buf += 1
@@ -620,19 +429,22 @@ class Bird(VecTask):
 			self.reset_idx(env_ids)
 
 		if self.viewer:
-			self.starts = self.wing_CoMs
-			ends_test = torch.zeros_like(self.starts)
-			ends_test[:,:,2] = 1
-			self.ends = self.starts + self.wing_forces
-			# self.ends = self.starts + ends_test
+			l_starts = self.lr_nodes 
+			l_ends = l_starts + 100*self.dt*self.ldT
+			# l_ends = l_starts + 1 * self.laux_data_pack["lift"] 
+			
+			r_starts = self.rr_nodes 
+			r_ends = r_starts + 100*self.dt*self.rdT
+			# r_ends = r_starts + 1 * self.raux_data_pack["lift"]
 
-			# print(self.ends)
-			verts = torch.stack([self.starts, self.ends], dim=2).cpu().numpy()
+			# submit debug line geometry
+			l_verts = torch.stack([l_starts, l_ends], dim=2).cpu().numpy()
+			r_verts = torch.stack([r_starts, r_ends], dim=2).cpu().numpy()
 			colors = np.zeros((self.num_envs * 4, 3), dtype=np.float32)
 			colors[..., 0] = 1.0
 			self.gym.clear_lines(self.viewer)
-			self.gym.add_lines(self.viewer, None, self.num_envs * 4, verts, colors)
-
+			self.gym.add_lines(self.viewer, None, self.num_envs * 4, l_verts, colors)
+			self.gym.add_lines(self.viewer, None, self.num_envs * 4, r_verts, colors)
 		
 		# self.compute_observations()
 		# self.compute_reward()
